4bda6654677d063f2fd89833c485acb65bee5258
Daniel Park <intheimageofyeshua@gmail.com>
2025-02-14 10:23:24 +0900

    Set Colors

--- SocialMediaMaui/MauiProgram.cs
+++ SocialMediaMaui/MauiProgram.cs
@@ -12,8 +12,10 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				//fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				//fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Ubuntu-Regular.ttf", "UbuntuRegular");
+				fonts.AddFont("Ubuntu-Bold.ttf", "UbuntuBold");
 			})
 			.UseMauiCommunityToolkit();
 
